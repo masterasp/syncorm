273811b7ec7e61a8b1074c039478fc991a2c3d9f
Ricardo Martínez <mrtnz.rcrd@gmail.com>
2018-11-09 08:24:00 +0100

    Posibilidad de excluir por accion de trigger
    nueva release v0.1.55

--- scripts/prepare_db.js
+++ scripts/prepare_db.js
@@ -2,81 +2,117 @@
 "use strict";
 
 var mysql = require('mysql'),
-	async = require('async'),
-	program = require('commander'),
-	prompt = require('prompt'),
-	prepareDB = require("./prepare_db_lib.js");
+    async = require('async'),
+    program = require('commander'),
+    prompt = require('prompt'),
+    prepareDB = require("./prepare_db_lib.js");
 
 var self = {};
 var db;
 
-async.series([function(cb) {
-	program
-		.version('0.0.1')
-		.option('-d, --db [db]', "Database to prepare (user@hostname/schema)" )
-		.option('-e, --excluded [tables]', "Excluded tables (separated by commas)")
-		.parse(process.argv);
+let options = {
+    exclude_all: [],
+    exclude_insert: [],
+    exclude_update: [],
+    exclude_delete: []
+}
 
-	if (!program.db) {
-		return cb(new Error("From Parameter not found"));
-	}
+async.series([function (cb) {
+    program
+        .version('0.1.56')
+        .option('-d, --db [db]', "Database to prepare (user@hostname/schema)")
+        .option('-e, --exclude_all [tables]', "Excluded all tables (separated by commas)")
+        .option('-i, --exclude_insert [tables]', "Excluded insert triggers on tables (separated by commas)")
+        .option('-u, --exclude_update [tables]', "Excluded update triggers on tables (separated by commas)")
+        .option('-r, --exclude_delete [tables]', "Excluded delete triggers on tables (separated by commas)")
+        .parse(process.argv);
+
+    if (!program.db) {
+        return cb(new Error("From Parameter not found"));
+    }
+
+    var arr;
+    arr = /([^:@]*)(:([^@]+))?@([^:\/]*)(:(.*))?\/(.*)/.exec(program.db);
+
+    if (!arr) {
+        return cb(new Error("Error parsing Database"));
+    }
+
+    if (program.exclude_all !== null && program.exclude_all !== undefined) {
+        if (typeof (program.exclude_all) !== "string") {
+            return cb(new Error("Parameter of exclude_all tables needs them separated by commas."));
+        }
+        options.exclude_all = program.exclude_all.split(',');
+    }
+
+    if (program.exclude_insert !== null && program.exclude_insert !== undefined) {
+        if (typeof (program.exclude_insert) !== "string") {
+            return cb(new Error("Parameter of exclude_insert tables needs them separated by commas."));
+        }
+        options.exclude_insert = program.exclude_insert.split(',');
+    }
+
+    if (program.exclude_update !== null && program.exclude_update !== undefined) {
+        if (typeof (program.exclude_update) !== "string") {
+            return cb(new Error("Parameter of exclude_update tables needs them separated by commas."));
+        }
+        options.exclude_update = program.exclude_update.split(',');
+    }
+
+    if (program.exclude_delete !== null && program.exclude_delete !== undefined) {
+        if (typeof (program.exclude_delete) !== "string") {
+            return cb(new Error("Parameter of exclude_delete tables needs them separated by commas."));
+        }
+        options.exclude_delete = program.exclude_delete.split(',');
+    }
 
 
-	var arr;
-	arr = /([^:@]*)(:([^@]+))?@([^:\/]*)(:(.*))?\/(.*)/.exec(program.db);
+    self.db = {
+        host: arr[4],
+        port: arr[6] ? arr[6] : 3306,
+        user: arr[1],
+        database: arr[7],
+        password: arr[2] ? arr[3] : null
+    };
 
-	if (!arr)  {
-		return cb( new Error("Error parsing Database"));
-	}
+    if (self.db.password !== null) {
+        return cb();
+    }
 
-	if(program.excluded && typeof (program.excluded) !== "string") {
-		return cb(new Error("Parameter of excluded tables needs them separated by commas."));
-	}
+    prompt.message = "";
+    prompt.delimiter = "";
 
-	self.db = {
-		host: arr[4],
-		port: arr[6] ? arr[6]: 3306,
-		user: arr[1],
-		database: arr[7],
-		password: arr[2] ? arr[3] : null
-	};
+    prompt.start();
+    prompt.get({
+        properties: {
+            pwddb: {
+                hidden: true,
+                description: arr[5] + " password: "
+            }
 
-	if (self.db.password !== null) {
-		return cb();
-	}
+        }
+    }, function (err, result) {
+        if (err) {
+            return cb(err);
+        }
+        self.db.password = result.pwddb;
+        cb();
+    });
+}, function (cb) {
+    db = mysql.createConnection(self.db);
+    db.connect(cb);
+}, function (cb) {
+    prepareDB(db, cb, options);
+}], function (err) {
+    if (err) {
+        console.log("Error: " + err);
+        console.log(err.stack);
+        program.outputHelp();
+        process.exit(-1);
 
-	prompt.message = "";
-	prompt.delimiter = "";
-
-	prompt.start();
-	prompt.get({ properties: {
-		pwddb: {
-			hidden:true,
-			description: arr[5]+ " password: "
-		}
-
-	}}, function(err, result) {
-		if (err) {
-			return cb(err);
-		}
-		self.db.password = result.pwddb;
-		cb();
-	});
-},function(cb) {
-	db= mysql.createConnection(self.db);
-	db.connect(cb);
-},function(cb) {
-	prepareDB(db,cb, program.excluded);
-}],function(err) {
-	if (err) {
-		console.log("Error: "+err);
-		console.log(err.stack);
-		program.outputHelp();
-		process.exit(-1);
-
-	} else {
-		process.exit(0);
-	}
+    } else {
+        process.exit(0);
+    }
 });
 
 
--- scripts/prepare_db_lib.js
+++ scripts/prepare_db_lib.js
@@ -2,167 +2,188 @@
 "use strict";
 
 var mysql = require('mysql'),
-	async = require('async');
+    async = require('async');
 
-function prepareTable(db, t, cb, excluded) {
-	console.log(t + ":Preparing " + (excluded ? ' - excluded!': ''));
-	var index_fields;
-	async.series([function(cb2) {
-		if(excluded) return cb2();
-		var q = "SHOW KEYS FROM `" + t + "` WHERE Key_name='PRIMARY' ";
-		db.query(q, function(err, rows) {
-			if (err) {
-				cb2(err);
-			}
-			rows.sort(function(a,b) {
-				return a.Seq_in_index - b.Seq_in_index;
-			});
-			index_fields = rows.map(function(row) {
-				return row.Column_name;
-			});
-			if (index_fields.length === 0) {
-				console.log("Table without primary key: "+ t);
-				return cb();
-			}
-			cb2();
-		});
-	},function(cb2) {
-		var q = "DROP TRIGGER IF EXISTS " + t+"_ai_dbops";
-		db.query(q, cb2);
-	},function(cb2) {
-		if(excluded) return cb2();
-		var q = "CREATE TRIGGER "+ t+"_ai_dbops AFTER INSERT ON "+ t + "\n"+
-				" FOR EACH ROW \n"+
-				" BEGIN \n"+
-				"    INSERT INTO dbops SET `user` = @dbops_user, \n" +
-				"                          `table`=  '" + t +"', \n" +
-				"                          `op` = 'INSERT', \n" +
-				"                          `datetime` = NOW(), \n" +
-				"                          `key` = CONCAT(";
-		var S="";
-		index_fields.forEach(function(f) {
-			if (S !== "") {
-				S += ",'|',";
-			}
-			S += "new.`" + f + "`";
-		});
-		q += S + "); \n" +
-				"END; \n";
-		db.query(q, cb2);
-	},function(cb2) {
-		var q = "DROP TRIGGER IF EXISTS " + t+"_au_dbops";
-		db.query(q, cb2);
-	},function(cb2) {
-		if(excluded) return cb2();
-		var S;
-		var q = "CREATE TRIGGER "+ t+"_au_dbops AFTER UPDATE ON "+ t + "\n"+
-				" FOR EACH ROW \n"+
-				" BEGIN \n"+
-				"    DECLARE oldkey VARCHAR(512); \n" +
-				"    DECLARE newkey VARCHAR(512); \n" +
-				"    SET @oldkey := CONCAT(";
-		S="";
-		index_fields.forEach(function(f) {
-			if (S !== "") {
-				S += ",'|',";
-			}
-			S += "old.`" + f + "`";
-		});
-		q +=  S + "); \n"+
-				"   SET @newkey := CONCAT(";
-		S="";
-		index_fields.forEach(function(f) {
-			if (S !== "") {
-				S += ",'|',";
-			}
-			S += "new.`" + f + "`";
-		});
-		q +=  S + "); \n"+
-				"    IF @newkey <> @oldkey THEN \n" +
-				"    	  INSERT INTO dbops SET `user` = @dbops_user, \n" +
-				"                          `table` =  '" + t +"', \n" +
-				"                          `op` = 'DELETE', \n" +
-				"                          `datetime` = NOW(),\n" +
-				"                          `key` = @oldkey; \n" +
-				"         INSERT INTO dbops SET `user` = @dbops_user, \n" +
-				"                               `table` =  '" + t +"', \n" +
-				"                               `op` = 'INSERT', \n" +
-				"                               `datetime` = NOW(),\n" +
-				"                               `key` = @newkey; \n" +
-				"    ELSE \n"+
-				"    	   INSERT INTO dbops SET `user` = @dbops_user, \n" +
-				"                          `table` =  '" + t +"', \n" +
-				"                          `op` = 'UPDATE', \n" +
-				"                          `datetime` = NOW(),\n" +
-				"                          `key` = @oldkey; \n" +
-				"    END IF; \n" +
-				"END; \n";
-		db.query(q, cb2);
-	},function(cb2) {
-		var q = "DROP TRIGGER IF EXISTS " + t+"_ad_dbops";
-		db.query(q, cb2);
-	},function(cb2) {
-		if(excluded) return cb2();
-		var q = "CREATE TRIGGER "+ t+"_ad_dbops AFTER DELETE ON "+ t + "\n"+
-				" FOR EACH ROW \n"+
-				" BEGIN \n"+
-				"    INSERT INTO dbops SET `user` = @dbops_user, \n" +
-				"                          `table` =  '" + t +"', \n" +
-				"                          `op` = 'DELETE', \n" +
-				"                          `datetime` = NOW(), \n" +
-				"                          `key` = CONCAT(";
-		var S="";
-		index_fields.forEach(function(f) {
-			if (S !== "") {
-				S += ",'|',";
-			}
-			S += "old.`" + f + "`";
-		});
-		q += S + "); \n" +
-				"END; \n";
-		db.query(q, cb2);
-	}], function(err) {
-		cb(err);
-	});
+function prepareTable(db, t, cb, exclude) {
+    console.log(t + ":Preparing " + (exclude.all ? ' - excluded!' : ''));
+    if (exclude.insert) {
+        console.log("Table " + t + " excluded from insert");
+    }
+    if (exclude.update) {
+        console.log("Table " + t + " excluded from update");
+    }
+    if (exclude.delete) {
+        console.log("Table " + t + " excluded from delete");
+    }
+    console.log()
+    var index_fields;
+    async.series([function (cb2) {
+        if (exclude.all) return cb2();
+        var q = "SHOW KEYS FROM `" + t + "` WHERE Key_name='PRIMARY' ";
+        db.query(q, function (err, rows) {
+            if (err) {
+                cb2(err);
+            }
+            rows.sort(function (a, b) {
+                return a.Seq_in_index - b.Seq_in_index;
+            });
+            index_fields = rows.map(function (row) {
+                return row.Column_name;
+            });
+            if (index_fields.length === 0) {
+                console.log("Table without primary key: " + t);
+                return cb();
+            }
+            cb2();
+        });
+    }, function (cb2) {
+        var q = "DROP TRIGGER IF EXISTS " + t + "_ai_dbops";
+        db.query(q, cb2);
+    }, function (cb2) {
+        if (exclude.all || exclude.insert) return cb2();
+        var q = "CREATE TRIGGER " + t + "_ai_dbops AFTER INSERT ON " + t + "\n" +
+            " FOR EACH ROW \n" +
+            " BEGIN \n" +
+            "    INSERT INTO dbops SET `user` = @dbops_user, \n" +
+            "                          `table`=  '" + t + "', \n" +
+            "                          `op` = 'INSERT', \n" +
+            "                          `datetime` = NOW(), \n" +
+            "                          `key` = CONCAT(";
+        var S = "";
+        index_fields.forEach(function (f) {
+            if (S !== "") {
+                S += ",'|',";
+            }
+            S += "new.`" + f + "`";
+        });
+        q += S + "); \n" +
+            "END; \n";
+        db.query(q, cb2);
+    }, function (cb2) {
+        var q = "DROP TRIGGER IF EXISTS " + t + "_au_dbops";
+        db.query(q, cb2);
+    }, function (cb2) {
+        if (exclude.all || exclude.update) return cb2();
+        var S;
+        var q = "CREATE TRIGGER " + t + "_au_dbops AFTER UPDATE ON " + t + "\n" +
+            " FOR EACH ROW \n" +
+            " BEGIN \n" +
+            "    DECLARE oldkey VARCHAR(512); \n" +
+            "    DECLARE newkey VARCHAR(512); \n" +
+            "    SET @oldkey := CONCAT(";
+        S = "";
+        index_fields.forEach(function (f) {
+            if (S !== "") {
+                S += ",'|',";
+            }
+            S += "old.`" + f + "`";
+        });
+        q += S + "); \n" +
+            "   SET @newkey := CONCAT(";
+        S = "";
+        index_fields.forEach(function (f) {
+            if (S !== "") {
+                S += ",'|',";
+            }
+            S += "new.`" + f + "`";
+        });
+        q += S + "); \n" +
+            "    IF @newkey <> @oldkey THEN \n" +
+            "    	  INSERT INTO dbops SET `user` = @dbops_user, \n" +
+            "                          `table` =  '" + t + "', \n" +
+            "                          `op` = 'DELETE', \n" +
+            "                          `datetime` = NOW(),\n" +
+            "                          `key` = @oldkey; \n" +
+            "         INSERT INTO dbops SET `user` = @dbops_user, \n" +
+            "                               `table` =  '" + t + "', \n" +
+            "                               `op` = 'INSERT', \n" +
+            "                               `datetime` = NOW(),\n" +
+            "                               `key` = @newkey; \n" +
+            "    ELSE \n" +
+            "    	   INSERT INTO dbops SET `user` = @dbops_user, \n" +
+            "                          `table` =  '" + t + "', \n" +
+            "                          `op` = 'UPDATE', \n" +
+            "                          `datetime` = NOW(),\n" +
+            "                          `key` = @oldkey; \n" +
+            "    END IF; \n" +
+            "END; \n";
+        db.query(q, cb2);
+    }, function (cb2) {
+        var q = "DROP TRIGGER IF EXISTS " + t + "_ad_dbops";
+        db.query(q, cb2);
+    }, function (cb2) {
+        if (exclude.all || exclude.delete) return cb2();
+        var q = "CREATE TRIGGER " + t + "_ad_dbops AFTER DELETE ON " + t + "\n" +
+            " FOR EACH ROW \n" +
+            " BEGIN \n" +
+            "    INSERT INTO dbops SET `user` = @dbops_user, \n" +
+            "                          `table` =  '" + t + "', \n" +
+            "                          `op` = 'DELETE', \n" +
+            "                          `datetime` = NOW(), \n" +
+            "                          `key` = CONCAT(";
+        var S = "";
+        index_fields.forEach(function (f) {
+            if (S !== "") {
+                S += ",'|',";
+            }
+            S += "old.`" + f + "`";
+        });
+        q += S + "); \n" +
+            "END; \n";
+        db.query(q, cb2);
+    }], function (err) {
+        cb(err);
+    });
 }
 
 
-function prepareDB(db, cb, excluded) {
-	var excluded = excluded ? excluded.split(',') : [];
-	async.series([function(cb2) {
-		var q = "DROP TABLE IF EXISTS `dbops`;";
-		db.query(q, cb2);
-	}, function(cb2) {
-		db.query("show full tables where Table_Type = 'BASE TABLE';", function(err, rows) {
-			if (err) {
-				return cb2(err);
-			}
-			var tables=rows.map(function(r) {
-				return r[Object.keys(r)[0]]; //returns 'someVal'
-			});
-			async.eachSeries(tables, function(t, cb3) {
-				prepareTable(db,t,cb3,(excluded.indexOf(t) !== -1));
-			}, function(err) {
-				cb2(err);
-			});
-		});
-	},function(cb2) {
-		var q = "CREATE  TABLE `dbops` (" +
-				" `id` INT NOT NULL AUTO_INCREMENT ," +
-				" `user` VARCHAR(45) NULL ," +
-				" `table` VARCHAR(45) NULL ," +
-				" `op` VARCHAR(8) NULL ," +
-				" `key` VARCHAR(256) NULL ," +
-				" `datetime` DATETIME NULL ," +
-				" PRIMARY KEY (`id`) );";
-		db.query(q, cb2);
-	}], function(err) {
-		cb(err);
-	});
+function prepareDB(db, cb, options) {
+    async.series([function (cb2) {
+        var q = "DROP TABLE IF EXISTS `dbops`;";
+        db.query(q, cb2);
+    }, function (cb2) {
+        db.query("show full tables where Table_Type = 'BASE TABLE';", function (err, rows) {
+            if (err) {
+                return cb2(err);
+            }
+            var tables = rows.map(function (r) {
+                return r[Object.keys(r)[0]]; //returns 'someVal'
+            });
+            async.eachSeries(tables, function (t, cb3) {
+
+                var exclude_all = (options.exclude_all.indexOf(t) !== -1);
+                var exclude_insert = (options.exclude_insert.indexOf(t) !== -1);
+                var exclude_update = (options.exclude_update.indexOf(t) !== -1);
+                var exclude_delete = (options.exclude_delete.indexOf(t) !== -1);
+
+                prepareTable(db, t, cb3, {
+                    all: exclude_all,
+                    insert: exclude_insert,
+                    update: exclude_update,
+                    delete: exclude_delete
+                });
+
+            }, function (err) {
+                cb2(err);
+            });
+        });
+    }, function (cb2) {
+        var q = "CREATE  TABLE `dbops` (" +
+            " `id` INT NOT NULL AUTO_INCREMENT ," +
+            " `user` VARCHAR(45) NULL ," +
+            " `table` VARCHAR(45) NULL ," +
+            " `op` VARCHAR(8) NULL ," +
+            " `key` VARCHAR(256) NULL ," +
+            " `datetime` DATETIME NULL ," +
+            " PRIMARY KEY (`id`) );";
+        db.query(q, cb2);
+    }], function (err) {
+        cb(err);
+    });
 }
 
 
-module.exports= prepareDB;
+module.exports = prepareDB;
 
 
 
